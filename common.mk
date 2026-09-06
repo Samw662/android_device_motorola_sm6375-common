b491764603cb4c2d79371a8587798e070f68c41f
samw662 <amadorsamir307@gmail.com>
2026-09-06 12:14:33 -0600

    sm6375-common: Use bangkk defconfig and disable ViPER4Android
    - Use device-specific bangkk_defconfig instead of generic holi-qgki
    - Comment out ViPER4Android inheritance (not available in source tree)

--- common.mk
+++ common.mk
@@ -386,5 +386,5 @@ PRODUCT_COPY_FILES += \
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm6375-common/sm6375-common-vendor.mk)
 
-#ViPER4Android FX
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+#ViPER4Android FX (disabled - not available in source tree)
+#$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
